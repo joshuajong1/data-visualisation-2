d4c272c70474d2f1ec5730f55480a1f3dd048544
joshuajong1 <90546610+joshuajong1@users.noreply.github.com>
2021-09-16 15:38:32 +0800

    Replace word cloud with donut chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,11 +1,13 @@
+
 var vg_1 = "bump_chart.vg.json";
 vegaEmbed("#bump_chart", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "word_cloud.vg.json";
-vegaEmbed("#word_cloud", vg_2).then(function (result) {
+var vg_2 = "donut_chart.vg.json";
+vegaEmbed("#donut_chart", vg_2).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vega.setRandom(vega.randomLCG(12));
+vega.setRandom(vega.randomLCG1(4));
+
