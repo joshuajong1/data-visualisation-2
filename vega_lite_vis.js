a9a6a6fac3d6cce8c192414290f8a376f4aa0bd2
joshuajong1 <90546610+joshuajong1@users.noreply.github.com>
2021-09-24 13:07:55 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -9,5 +9,40 @@ vegaEmbed("#donut_chart", vg_2).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vega.setRandom(vega.randomLCG1(4));
+var vg_3 = "word_cloud.vg.json";
+vegaEmbed("#word_cloud", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "map.vg.json";
+vegaEmbed("#map", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "ring_chart.vg.json";
+vegaEmbed("#radial_plot", vg_6).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "heatmap_icu_usage.vg.json";
+vegaEmbed("#heatmap-icu", vg_7).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8 = "heatmap_cases.vg.json";
+vegaEmbed("#heatmap-cases", vg_8).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_9 = "deaths_isotype.vg.json";
+vegaEmbed("#isotype-death", vg_9).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vega.setRandom(vega.randomLCG1(2));
 
